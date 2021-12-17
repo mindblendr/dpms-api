27ab4eba2f2c2ce3a34d7dbfa7854f451338ffb7
Mindblendr <mindblendr@gmail.com>
2021-12-18 01:20:41 +0800

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-async-await");
+
+const schema = mongoose.Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true,
+		dropDups: true
+	},
+	password: String,
+	pin: Number,
+	type: {
+		type: String,
+		default: 'user'
+	},
+	firstname: String,
+	lastname: String,
+	status: {
+		type: Number,
+		default: 1
+	}
+}, { 
+	timestamps: true,
+	collection: 'user'
+ });
+
+schema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('User', schema);
